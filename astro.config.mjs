3736da398104daf9333a26d22570c4bf625bd572
Mojhame <fabian.zegarravasquez.777@gmail.com>
2026-03-21 17:18:07 -0500

    prueba5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://alejandro.github.io',
+    site: 'https://Mojhame.github.io',
 });
